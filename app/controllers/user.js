3772a8c44e5bf7cf481da3bd3cbd315711f552be
RicharDarcy <richard.darcy@escen.fr>
2022-08-26 09:53:20 +0200

    check si les deux passwords rentres sont les memes

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -49,11 +49,16 @@ module.exports = {
             county,
             role,
             password,
+            password2,
         } = req.body;
 
-        if (!name || !email || !city || !county || !role || !password) {
+        if (!name || !email || !city || !county || !role || !password || !password2) {
             res.status(400);
-            throw new Error('Please add all fields');
+            throw new Error('Please file all fields');
+        }
+        if (password !== password2) {
+            res.status(400);
+            throw new Error('Passwords not the same');
         }
 
         // Check if user exists
